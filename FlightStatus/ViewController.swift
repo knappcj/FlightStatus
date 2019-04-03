820453273ad420e0dffa6b65f7378b84779e827c
Chris Knapp <knappce@bc.edu>
2019-04-03 08:59:25 -0400

    Initial Commit

---
+++ FlightStatus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightStatus
+//
+//  Created by Christopher Knapp on 4/3/19.
+//  Copyright © 2019 Christopher Knapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
